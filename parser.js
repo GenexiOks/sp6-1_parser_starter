a2a0ec76c973f672bcc82ea59f1665852e55c1b2
Pavel Zhukovsky <GenexiOks@yandex.by>
2025-08-12 09:22:35 +0300

    12/08/25 home

--- parser.js
+++ parser.js
@@ -17,6 +17,8 @@ function parsePage() {
 
   let discount = oldPrice - price;
 
+  let discountPercent = (discount / oldPrice) * 100;
+
   let currency = document.querySelector('.price').textContent.split('');
 
   function currencyFn(data) {
@@ -71,9 +73,7 @@ function parsePage() {
     cardData.description = card.querySelector('p').textContent;
     cardData.image = card.querySelector('img').getAttribute('src');
     cardData.price = card.querySelector('b').textContent.match(/\d+/)[0];
-    cardData.currency = card.querySelector('b').textContent.includes('₽')
-      ? 'RUB'
-      : 'Невозможно определить валюту!';
+    cardData.currency = card.querySelector('b').textContent.includes('₽') ? 'RUB' : 'Невозможно определить валюту!';
     suggestedArray.push(cardData);
   });
 
@@ -82,63 +82,56 @@ function parsePage() {
   let reviewsDom = document.querySelectorAll('.reviews article');
 
   reviewsDom.forEach((card) => {
-    console.log(cardData);
+    cardData = {};
+    card.querySelectorAll('.rating span').forEach((elem) => {
+      if (!cardData.rating) {
+        cardData.rating = 0;
+      }
+      if (elem.classList.value == 'filled') {
+        cardData.rating++;
+      }
+    });
+
+    cardData.author = { avatar: card.querySelector('.author img').getAttribute('src'), name: card.querySelector('.author span').textContent };
+    cardData.title = card.querySelector('.title').textContent;
+    cardData.description = card.querySelector('p').textContent;
+    cardData.date = card.querySelector('i').textContent.replace(/[^\d]+/g, '.');
     reviewsArray.push(cardData);
   });
 
-  console.log(reviewsDom);
-
   let meta = {
-    title: document.querySelector('title')?.textContent.includes('') || null,
-    description:
-      document
-        .querySelector('meta[name="description"]')
-        ?.getAttribute('content') || null,
+    title: document.querySelector('title')?.textContent.split(' ').slice(0, 2).join(' ') || null,
+    description: document.querySelector('meta[name="description"]')?.getAttribute('content') || null,
     keywords:
       document
         .querySelector("meta[name='keywords']")
         ?.getAttribute('content')
-        .split(',') || null,
+        .split(',')
+        .map((str) => str.trim()) || null,
     language: document.querySelector('html')?.getAttribute('lang') || null,
     opengraph: {
-      title:
-        document
-          .querySelector("meta[property='og:title']")
-          ?.getAttribute('content')
-          .split(' ', 2)
-          .join(' ') || null,
-      image:
-        document
-          .querySelector("meta[property='og:image']")
-          ?.getAttribute('content') || null,
-      type:
-        document
-          .querySelector("meta[property='og:type']")
-          ?.getAttribute('content') || null,
+      title: document.querySelector("meta[property='og:title']")?.getAttribute('content').split(' ', 2).join(' ') || null,
+      image: document.querySelector("meta[property='og:image']")?.getAttribute('content') || null,
+      type: document.querySelector("meta[property='og:type']")?.getAttribute('content') || null,
     },
   };
 
   product = {
     id: document.querySelector('.product')?.getAttribute('data-id') || null,
-    name:
-      document.querySelector('title')?.textContent.split(' ', 2).join(' ') ||
-      null,
+    name: document.querySelector('title')?.textContent.split(' ', 2).join(' ') || null,
     isLiked: document.querySelector('.like.active') ? true : false,
     tags: {
-      category:
-        document.querySelector('.green')?.textContent.split(',') || null,
+      category: document.querySelector('.green')?.textContent.split(',') || null,
       discount: document.querySelector('.red')?.textContent.split(',') || null,
       label: document.querySelector('.blue')?.textContent.split(',') || null,
     },
     price: price,
     oldPrice: oldPrice,
     discount: discount,
-    discountPercent: `${(discount / oldPrice) * 100}%`
-      ? `${(discount / oldPrice) * 100}%`
-      : '0%',
+    discountPercent: `${discountPercent.toFixed(2)}%` ? `${discountPercent.toFixed(2)}%` : '0%',
     currency: currency,
     properties: propertiesObj,
-    description: document.querySelector('.description').innerHTML,
+    description: document.querySelector('.description').innerHTML.trim(),
     images: imagesArray,
   };
 
