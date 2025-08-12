6a62251bf2100693e4029672e9ff9e0077e2e49e
Pavel <Pashaz1994@gmail.com>
2025-08-12 12:11:32 +0300

    12/08.25 work

--- parser.js
+++ parser.js
@@ -40,6 +40,10 @@ function parsePage() {
 
   currencyFn(currency);
 
+  let description = document.querySelector('.description');
+
+  description.querySelector('h3').removeAttribute('class');
+
   let propertiesObj = {};
 
   let properties = document.querySelectorAll('.properties li');
@@ -73,7 +77,9 @@ function parsePage() {
     cardData.description = card.querySelector('p').textContent;
     cardData.image = card.querySelector('img').getAttribute('src');
     cardData.price = card.querySelector('b').textContent.match(/\d+/)[0];
-    cardData.currency = card.querySelector('b').textContent.includes('₽') ? 'RUB' : 'Невозможно определить валюту!';
+    cardData.currency = card.querySelector('b').textContent.includes('₽')
+      ? 'RUB'
+      : 'Невозможно определить валюту!';
     suggestedArray.push(cardData);
   });
 
@@ -92,7 +98,10 @@ function parsePage() {
       }
     });
 
-    cardData.author = { avatar: card.querySelector('.author img').getAttribute('src'), name: card.querySelector('.author span').textContent };
+    cardData.author = {
+      avatar: card.querySelector('.author img').getAttribute('src'),
+      name: card.querySelector('.author span').textContent,
+    };
     cardData.title = card.querySelector('.title').textContent;
     cardData.description = card.querySelector('p').textContent;
     cardData.date = card.querySelector('i').textContent.replace(/[^\d]+/g, '.');
@@ -100,8 +109,16 @@ function parsePage() {
   });
 
   let meta = {
-    title: document.querySelector('title')?.textContent.split(' ').slice(0, 2).join(' ') || null,
-    description: document.querySelector('meta[name="description"]')?.getAttribute('content') || null,
+    title:
+      document
+        .querySelector('title')
+        ?.textContent.split(' ')
+        .slice(0, 2)
+        .join(' ') || null,
+    description:
+      document
+        .querySelector('meta[name="description"]')
+        ?.getAttribute('content') || null,
     keywords:
       document
         .querySelector("meta[name='keywords']")
@@ -110,25 +127,41 @@ function parsePage() {
         .map((str) => str.trim()) || null,
     language: document.querySelector('html')?.getAttribute('lang') || null,
     opengraph: {
-      title: document.querySelector("meta[property='og:title']")?.getAttribute('content').split(' ', 2).join(' ') || null,
-      image: document.querySelector("meta[property='og:image']")?.getAttribute('content') || null,
-      type: document.querySelector("meta[property='og:type']")?.getAttribute('content') || null,
+      title:
+        document
+          .querySelector("meta[property='og:title']")
+          ?.getAttribute('content')
+          .split(' ', 2)
+          .join(' ') || null,
+      image:
+        document
+          .querySelector("meta[property='og:image']")
+          ?.getAttribute('content') || null,
+      type:
+        document
+          .querySelector("meta[property='og:type']")
+          ?.getAttribute('content') || null,
     },
   };
 
   product = {
     id: document.querySelector('.product')?.getAttribute('data-id') || null,
-    name: document.querySelector('title')?.textContent.split(' ', 2).join(' ') || null,
+    name:
+      document.querySelector('title')?.textContent.split(' ', 2).join(' ') ||
+      null,
     isLiked: document.querySelector('.like.active') ? true : false,
     tags: {
-      category: document.querySelector('.green')?.textContent.split(',') || null,
+      category:
+        document.querySelector('.green')?.textContent.split(',') || null,
       discount: document.querySelector('.red')?.textContent.split(',') || null,
       label: document.querySelector('.blue')?.textContent.split(',') || null,
     },
     price: price,
     oldPrice: oldPrice,
     discount: discount,
-    discountPercent: `${discountPercent.toFixed(2)}%` ? `${discountPercent.toFixed(2)}%` : '0%',
+    discountPercent: `${discountPercent.toFixed(2)}%`
+      ? `${discountPercent.toFixed(2)}%`
+      : '0%',
     currency: currency,
     properties: propertiesObj,
     description: document.querySelector('.description').innerHTML.trim(),
